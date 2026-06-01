3d8637466829073901802230c735db8ece74c0e9
Lodesthard <133907854+Lodesthard@users.noreply.github.com>
2026-06-01 07:01:10 +0200

    début 3e étage

--- Assets/Scripts/VRMenuSettings.cs
+++ Assets/Scripts/VRMenuSettings.cs
@@ -16,8 +16,11 @@ public class VRMenuSettings : MonoBehaviour
     {
         if (sliderSize != null)
         {
-            sliderSize.onValueChanged.AddListener(OnSizeChanged);
             sliderSize.value = xrOrigin.localScale.x;
+            // La taille ne s'applique qu'au RELÂCHEMENT du slider, pas pendant le drag.
+            var rel = sliderSize.GetComponent<SliderApplyOnRelease>();
+            if (rel == null) rel = sliderSize.gameObject.AddComponent<SliderApplyOnRelease>();
+            rel.Released += OnSizeChanged;
         }
 
         if (sliderSpeed != null)
